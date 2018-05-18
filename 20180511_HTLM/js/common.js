b015a78cba2f5f8658cb76557f36fc20a473c1e4
安部将道 <abe0115kirigamine@gmail.com>
2018-05-18 11:50:41 +0900

    no message

--- 20180511_HTLM/js/common.js
+++ 20180511_HTLM/js/common.js
@@ -1,30 +1,29 @@
-console.log('test')
-
-var test = 'test1';
-console.log(test);
+var test = 'テスト';
+console.log(test, typeof test);
 var num = 1;
-console.log(num);
-var num2 = 2;
-console.log(num + num2);
+console.log(num, typeof num);
+var num2 = '2';
+console.log(num2, typeof num2);
+var bool = true;
+console.log(bool, typeof bool);
+console.log(false, typeof false);
 
-console.log(test + num2)
-console.log(1 + 1)
-console.log(1 - 1)
-console.log(2 * 3)// 掛け算
-console.log(4 / 2)// 割り算
-console.log(9 % 2)// 余り
+// 比較演算子
+console.log(1 === 1);
+console.log(1 !== 1);
+console.log(1 < 2);
+console.log(1 > 2);
+console.log(1 >= 1);
+console.log(1 <= 1);
 
-num += 2
-num += 2
-num += 2
-num += 2
-console.log(num)
-num -= 2 // num = num - 2と一緒
-// 上と一緒の意味
-num = num +　2
-num = num -　2
+// コメント 
+/*
+　複数行コメント
+*/
 
-num++ // num = num + 1と一緒
-num-- // num = num - 1と一緒
-num++
-console.log(num)
+//　論理演算子
+var num = 10;
+console.log(num > 5 && num < 20);
+
+var num2 = Math.floor(Math.random() * 10); //ランダムに0から9までの数を入れる
+console.log(num2, num2 === 5 || num2 === 3); //その数字と比較して5もしくは3の数字だった場合true
